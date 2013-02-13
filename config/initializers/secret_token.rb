52909657a8d3ec89e53aa65f1526491c58fa3105
Fraser Newton <fraser.newton@gmail.com>
2013-02-13 16:20:02 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MigrationError::Application.config.secret_token = 'd99729fe12f21ca38d73f642aa5fcf3358690a0a1bc46abb76811b6f33b77fb271163c286bddb226e548af5213f21a92f9c66c8ab0c90406ccf3355759fdd7ee'
